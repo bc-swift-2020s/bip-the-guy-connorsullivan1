7d8ed4af973a189fa5dd80a501f991894a13ce10
Connor Sullivan <sulliamr@bc.edu>
2020-02-03 22:14:08 -0500

    Initial Commit

---
+++ BipTheGuy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BipTheGuy
+//
+//  Created by Connor on 2/3/20.
+//  Copyright © 2020 Connor Sullivan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
